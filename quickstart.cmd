f05c0abf1daf25ce73427e23afe3d40950d9fcc7
Bryan <bryanjappleby@gmail.com>
2021-09-23 15:50:41 +0300

    Quickstart script for windows

---
+++ quickstart.cmd
@@ -0,0 +1,8 @@
+cd app\src
+python3 server.py
+start chrome http://localhost:5000
+start start microsoft-edge:http://localhost:5000
+start firefox http://localhost:5000
+start opera http://localhost:5000
+
+cmd \k
